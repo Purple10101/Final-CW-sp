d452dc7706df6157b37bbcb01feb459abe9b4e87
Purple10101 <116915084+Purple10101@users.noreply.github.com>
2024-04-30 18:25:39 +0100

    Update MyProg.py

--- MyProg.py
+++ MyProg.py
@@ -37,13 +37,14 @@ from CircResultsExporter import CircResultsExporter
 
 
 def errorexporter(file_path):
+    # If an error is encountered that results in the circuit provided
+    # not being simulatable, then an empty csv file is to be exported.
     print("An empty CSV file is being exported")
     with open(file_path, 'w', newline='') as output_file:
             output_file.write() 
 
-# Usage
 
-
+# Store the command line arguments 
 if len(sys.argv) != 3:
     print("Usage: python MyProg.py <input_file> <output_file>")
     sys.exit(1)
@@ -51,19 +52,26 @@ if len(sys.argv) != 3:
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 Txt_Data = DataExtract(input_file)
+# attempt to generate any frequency sweep
 try:
+    # attempt to generate a linear frequency sweep by accesing the 'Fstart', 'Fend', 'Nfreqs' keys
     try:
         freq = numpy.linspace(Txt_Data.formatted_Term_Values['Fstart'], Txt_Data.formatted_Term_Values['Fend'], Txt_Data.formatted_Term_Values['Nfreqs'])
+    # This will occur when the 'Fstart', 'Fend', 'Nfreqs' keys do not exist
+    # In this case attempt to generate a log frequency sweep by accesing the 'LFstart', 'LFend', 'Nfreqs' keys
     except Exception:
         freq = numpy.logspace(numpy.log10(Txt_Data.formatted_Term_Values['LFstart']), numpy.log10(Txt_Data.formatted_Term_Values['LFend']), Txt_Data.formatted_Term_Values['Nfreqs'])
+# This will occur when there is no frequecny information to access.
 except Exception:
     errorexporter(output_file)
 
+# Iterate through each line in formatted_Circ_Values, each of which represent a component
 frequencies = freq
-
 components = []
 for component_data in Txt_Data.formatted_Circ_Values:
+    # attempt to generate any component
     try:
+        # attempt to generate a component as an instance of Impedance
         try:
             comp = Impedance(
                 Pin1=component_data['n1'], 
@@ -71,6 +79,8 @@ for component_data in Txt_Data.formatted_Circ_Values:
                 Value=component_data['value'], 
                 Type=component_data['type']
             )
+        # This will occr when a component cannot be represented as an instance of Impedance
+        # In this case # attempt to generate a component as an instance of FreqDepImpedence
         except ComponentTypeException:
             comp = FreqDepImpedence(
                 Pin1=component_data['n1'], 
@@ -79,11 +89,15 @@ for component_data in Txt_Data.formatted_Circ_Values:
                 Type=component_data['type'], 
                 Freq=frequencies
             )
+    # This will occur when a component is misconnected (n1 = n2)
     except ComponentConnectionException:
+        # In this case simply skip this component!
         continue
     components.append(comp)
 
+# Is any components have been made
 if len(components) != 0:
+    # attempt to perform outputs calculations and exportation
     try:
         circuit = Circ(components_list= components, Freq= frequencies, LoadRes= Txt_Data.formatted_Term_Values['RL'], Vth= Txt_Data.formatted_Term_Values['VT'], Rs= Txt_Data.formatted_Term_Values['RS'])
         param = Txt_Data.formatted_Outputs
